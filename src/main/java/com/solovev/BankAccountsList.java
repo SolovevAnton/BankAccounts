f0d4e8d79d7402123191044720888e9395780464
Solovev Anton <solovev.anton93@gmail.com>
2023-04-19 10:52:52 +0300

    after comments

--- src/main/java/com/solovev/BankAccountsList.java
+++ src/main/java/com/solovev/BankAccountsList.java
@@ -4,24 +4,29 @@ import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+//TODO комментарии лишние убрать, жокументируемые комментарии сделать над методами и комментарии
+
 /*Class stores all bank accounts. Also, they can be sorted
-* duplicates allowed*/
+ * duplicates allowed*/
 public class BankAccountsList {
     /*Collection to store bank accounts*/
     private ArrayList<BankAccount> accounts;
     /*
-    * List of Comparators used to sort the collection
-    * These comparators are described like constants
-    * later cam be put in Map?? or ENUM??
-    * */
+     * List of Comparators used to sort the collection
+     * These comparators are described like constants
+     * later cam be put in Map?? or ENUM??
+     * */
+
+    //TODO компараторы должны быть по факту расположены в тех местах где востребованы
     static public final Comparator<BankAccount> BY_NUM = Comparator.comparing(BankAccount::getNUM);
     static public final Comparator<BankAccount> BY_NAME = Comparator.comparing(BankAccount::getLastName);
     static public final Comparator<BankAccount> BY_DATE = Comparator.comparing(BankAccount::getDATE);
     /*Comparator used to sort by all fields in the order: Num, Date, Name
-    * */
+     * */
     static public final Comparator<BankAccount> BY_ALL = BY_NUM.thenComparing(BY_DATE).thenComparing(BY_NAME);
-   /*if null appears as a BankAccount object it is just thrown away*/
-    public BankAccountsList(BankAccount...accounts){
+
+    /*if null appears as a BankAccount object it is just thrown away*/
+    public BankAccountsList(BankAccount... accounts) {
         this.accounts = Arrays
                 .stream(accounts)
                 .filter(Objects::nonNull)
@@ -29,41 +34,30 @@ public class BankAccountsList {
     }
 
     /*method sorts accounts by given comparator*/
-    public void sort(Comparator<BankAccount> comparator){
+    public void sort(Comparator<BankAccount> comparator) {
         accounts.sort(comparator);
     }
+
     /*methods that search account for given numberOfAccount, Name, or creation date.
-    * @return List<BankAccount> consisting of all found accounts, or empty collection if nothing was found*/
-    public List<BankAccount> search(long numberOfAccount){
-        return searchFor(b-> b.getNUM() == numberOfAccount);
+     * @return List<BankAccount> consisting of all found accounts, or empty collection if nothing was found*/
+    public List<BankAccount> search(long numberOfAccount) {
+        return search(b -> b.getNUM() == numberOfAccount);
     }
-    public List<BankAccount> search(Date dateOfCreation){
-        return searchFor(b-> b.getDATE().equals(dateOfCreation));
+
+    public List<BankAccount> search(Date dateOfCreation) {
+        return search(b -> b.getDATE().equals(dateOfCreation));
     }
-    public List<BankAccount> search(String lastName){
-        return searchFor(b-> b.getLastName().equals(lastName));
+
+    public List<BankAccount> search(String lastName) {
+        return search(b -> b.getLastName().equals(lastName));
     }
+
     /*helper method to return List of bank accounts based on the given Predicate*/
-    private List<BankAccount> searchFor(Predicate<BankAccount> type){
+    private List<BankAccount> search(Predicate<BankAccount> type) {
         return accounts
                 .stream()
                 .filter(type)
                 .collect(Collectors.toList());
-    }
-    public ArrayList<BankAccount> getAccounts() {
-        return new ArrayList<>(accounts);
-    }
-
-    public void setAccounts(ArrayList<BankAccount> accounts) {
-        if(accounts == null) { throw new NullPointerException("accounts can't be null");}
-        this.accounts = accounts;
-    }
-
-    @Override
-    public String toString() {
-        return "BankAccountsList{" +
-                "accounts=" + accounts +
-                '}';
     }
 
     @Override
@@ -73,11 +67,18 @@ public class BankAccountsList {
 
         BankAccountsList that = (BankAccountsList) o;
 
-        return getAccounts() != null ? getAccounts().equals(that.getAccounts()) : that.getAccounts() == null;
+        return Objects.equals(accounts, that.accounts);
     }
 
     @Override
     public int hashCode() {
-        return getAccounts() != null ? getAccounts().hashCode() : 0;
+        return accounts != null ? accounts.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccountsList{" +
+                "accounts=" + accounts +
+                '}';
     }
 }
--- src/main/java/com/solovev/Test.java
+++ src/main/java/com/solovev/Test.java
@@ -5,38 +5,43 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.util.List;
+
+//TODO раскидать классы по пакетам
 
 /*Class for testing purposes*/
-public class Test {
+public class Test { //TODO заменить на Main
 
-    static public void main(String args[])  throws ParseException {
-    BankAccount[] accounts= {
-            new BankAccount(1,2,"anton",10,"1.1.2022",5),
-            new BankAccount(1,2,"anton",10,"1.1.2022",5),
-            new BankAccount(-1,-2,"anna",-10,"12.12.2022",-5),
-            new BankAccount(1,2,"AnnaLate",1,"13.12.2022",-5)
-    };
-        Comparator[] comparators ={
+    static public void main(String[] args) throws ParseException {
+        BankAccount[] accounts = {
+                new BankAccount(1, 2, "anton", 10, "1.1.2022", 5),
+                new BankAccount(1, 2, "anton", 10, "1.1.2022", 5),
+                new BankAccount(-1, -2, "anna", -10, "12.12.2022", -5),
+                new BankAccount(1, 2, "AnnaLate", 1, "13.12.2022", -5)
+        };
+        List<Comparator<BankAccount>> comparators = List.of( //TODO Comparator - это шаблон, нужно указывать тип данных
                 BankAccountsList.BY_NUM,
                 BankAccountsList.BY_DATE,
                 BankAccountsList.BY_NAME,
                 BankAccountsList.BY_ALL
-        };
-    BankAccountsList acc= new BankAccountsList(accounts);
+        );
+        BankAccountsList acc = new BankAccountsList(accounts);
 
-    for (Comparator c : comparators) {
-        acc.sort(c);
-        System.out.println(acc + "\n");
-    }
+        for (Comparator<BankAccount> c : comparators) { //TODO Comparator - это шаблон, нужно указывать тип данных
+            acc.sort(c);
+            System.out.println(acc + "\n");
+        }
 
-    SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        //TODO если в метод сортировки подать вместо компаратора null, то тогда сортировка может идти методом compareTo
 
-    System.out.println(acc.search(-1));
-    System.out.println(acc.search(5l));
-    System.out.println(acc.search(dateFormat.parse("12.12.2022")));
-    System.out.println(acc.search(dateFormat.parse("5.12.1999")));
-    System.out.println(acc.search("anton"));
-    System.out.println(acc.search("Anton"));
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+
+        System.out.println(acc.search(-1));
+        System.out.println(acc.search(5L));
+        System.out.println(acc.search(dateFormat.parse("12.12.2022")));
+        System.out.println(acc.search(dateFormat.parse("5.12.1999")));
+        System.out.println(acc.search("anton"));
+        System.out.println(acc.search("Anton"));
 
 
     }
